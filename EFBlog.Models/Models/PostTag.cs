a08ea4ced4072b40d8e1f22737169b9994afa5a6
Vinicius <vini383@gmail.com>
2024-02-21 17:05:41 -0300

    first commit

---
+++ EFBlog.Models/Models/PostTag.cs
@@ -0,0 +1,9 @@
+﻿namespace EFBlog.Models.Models
+{
+    public class PostTag
+    {
+        public int PostId { get; set; }
+        public int TagId { get; set; }
+        
+    }
+}
---
+++ EFBlog.Models/Models/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace EFBlog.Models.Models
+{
+    public class UserRole
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
